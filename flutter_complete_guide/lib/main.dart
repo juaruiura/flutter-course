ea248c755e0f7e35e57775d1302c24a92eaba78c
Juan Ruiz <ruizuranojuanmanuel@gmail.com>
2021-04-19 19:11:35 +0100

    new question widget to separate and organize code

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,14 +1,56 @@
-
 import 'package:flutter/material.dart';
+
+import './question.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  var questions = [
+    "What's your favourite color?",
+    "What's your favourite animal?",
+  ];
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          children: [
+            Question(questions[questionIndex]),
+            ElevatedButton(
+              child: Text("Answer 1"),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text("Answer 2"),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text("Answer 3"),
+              onPressed: answerQuestion,
+            ),
+          ]
+        ),
+      ),
     );
+  }
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex < questions.length-1 ? questionIndex++ : questionIndex = 0;
+    });
   }
 }
